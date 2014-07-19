f03b10a55b8a9f088bfb51d808d3e26976d03982
joshuabg <joshuabg@verizon.net>
2014-07-19 18:19:35 -0400

    Update aicp.mk

--- aicp.mk
+++ aicp.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common cyanogenmod stuff.
+# Inherit some common aicp stuff.
 $(call inherit-product-if-exists, vendor/aicp/configs/common_tablet.mk)
 
 #TARGET_SCREEN_WIDTH := 600
